afb4199a4d9aa850000abc2873c5944cfe277aab
ulyana-akinfina <ulyana.akinfina22@gmail.com>
2023-01-08 20:55:59 +0200

    Add lesson_06

--- lesson_06/classwork_03.py
+++ lesson_06/classwork_03.py
@@ -1,29 +1,15 @@
 """
-Использую функцию из предыдущей задачи, написать программу игру Блэкджек,
-т.е. реализовать цикл в котором на каждом ходу у игрока спрашивается,
-достать ли следующую карту, в случае положительного ответа  - вытягивать случайную карту.
-Игра заканчивается если игрок отказывается брать карту, либо сумма его карт больше 21-го.
-
-
+Написать функцию которая возвращают случайным образом одну
+карту из стандартной колоды в 36 карт, где на первом месте номинал карты номинал
+(6 - 10, J, D, K, A), а на втором название масти (Hearts, Diamonds, Clubs, Spades).
 """
 
+import random
 
+def card_random():
+    test_list = ["6", "7", "8", "9", "10", "J", "D", "K", "A"]
+    test_list_2 = ["Hearts", "Diamonds", "Clubs", "Spades"]
 
+    return print(random.choice(test_list) + "-" + random.choice(test_list_2))
 
-import  random
-
-summa = 0
-while summa < 21:
-    test_list = [6, 7, 8, 9, 10, "J", "D", "K", "A", ]
-    card = random.choice(test_list)
-    if card == range(6, 11):
-        summa += card
-    elif card == "J" or "D" or "K":
-        summa += 10
-    else:
-        if summa >= 21:
-            summa += 1
-        else:
-            summa += 11
-print(summa)
-
+card_random()
