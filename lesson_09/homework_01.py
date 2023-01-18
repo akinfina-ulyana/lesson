a1b245b4e18a55543f9f65fff6ae581215812803
ulyana-akinfina <ulyana.akinfina22@gmail.com>
2023-01-18 16:53:14 +0200

    Add lesson_08

--- lesson_09/homework_01.py
+++ lesson_09/homework_01.py
@@ -9,9 +9,9 @@
 from lesson_09.lib_2 import Point
 
 
-class Triangle(Point):
+class Triangle:
 
-    def __init__(self, x1, y1, x2, y2 ,x3, y3):
+    def __init__(self, x1, x2, x3, y1, y2, y3):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
@@ -19,22 +19,26 @@ class Triangle(Point):
         self.x3 = x3
         self.y3 = y3
 
-    def area_triangl(self, x1, x2, x3, y1, y2, y3):
+    def area_triangl(self):
         a = ((self.x2 - self.x1) + (self.y2 - self.y1)) ** 0.5
         b = ((self.x3 - self.x2) + (self.y3 - self.y3)) ** 0.5
         c = ((self.x3 - self.x1) + (self.y3 - self.y1)) ** 0.5
 
-        p = (a + b + c)
+        p = (a + b + c)/2
         s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
-
+        print(s)
         return s
 
-    def perimetr_triangl(self, x1, x2, x3, y1, y2, y3):
-        a = ((self.x2 - self.x1) + (self.y2 - self.y1)) ** 0.5
-        b = ((self.x3 - self.x2) + (self.y3 - self.y3)) ** 0.5
-        c = ((self.x3 - self.x1) + (self.y3 - self.y1)) ** 0.5
+    def perimetr_triangl(self):
+        a = ((abs(self.x2 - self.x1)) + (abs(self.y2 - self.y1))) ** 0.5
+        b = ((abs(self.x3 - self.x2)) + (abs(self.y3 - self.y3))) ** 0.5
+        c = ((abs(self.x3 - self.x1)) + (abs(self.y3 - self.y1))) ** 0.5
 
         perimetr = a + b + c
-        return perimetr
+        perimetr = print(float(perimetr))
 
+        return a
+
+triangle = Triangle(3, 6, 11, 3, 8, 4)
+triangle.perimetr_triangl()
 
