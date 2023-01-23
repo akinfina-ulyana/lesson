b1661a4d50b090f9bd245e66fed28712a7c41ced
ulyana-akinfina <ulyana.akinfina22@gmail.com>
2023-01-23 15:00:48 +0200

    Add lesson_08

--- lesson_10/homework_01.py
+++ lesson_10/homework_01.py
@@ -1,12 +1,19 @@
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 result = 0
 current = 1
-s = int(input())
+s = int(input("Введите число: "))
 q = s
-m = int(input())
+m = int(input("Введите число: "))
 while current <= m:
     result += s
     s = s * 10 + q
     current += 1
-    print(result)
+logger.info(result)
 
 
