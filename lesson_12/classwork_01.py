b0c99382a901420e174ca672e9cb034d80093730
ulyana-akinfina <ulyana.akinfina22@gmail.com>
2023-01-30 21:55:23 +0200

    Add lesson_10

---
+++ lesson_12/classwork_01.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from sqlalchemy import create_engine
+from sqlalchemy_utils import create_database, database_exists
+
+DB_PATH = Path(__file__).resolve().parent / "my_database.sqlite3"
+DB_ECHO = True
+
+if __name__ == "__main__":
+
+    engine = create_engine(f"sqlite:////{DB_PATH}", echo=DB_ECHO)
+    if not database_exists(engine.url):
+        create_database(engine.url)
