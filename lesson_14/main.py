67436d4f3a2de6e7de7df69b785d8015e3317eb4
ulyana-akinfina <ulyana.akinfina22@gmail.com>
2023-02-08 01:26:03 +0200

    Add lesson_14

--- lesson_14/main.py
+++ lesson_14/main.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database, database_exists
-from lesson_14.utils import create_tables
+from lesson_14.utils import create_tables, create_user, find_user, user_check, user_address, user_profile
 
 DB_USER = "ulitka"
 DB_PASSWORD = "ulitka"
@@ -21,4 +21,45 @@ if __name__ == "__main__":
 
     session = create_tables(engine)
 
-    logger.info("Hello")
+
+    menu = """
+        1. создать пользователя
+        2. Найти пользователя
+        3. exit
+        """
+    while True:
+        logger.info(menu)
+        choice = input("Введите функцию: 1- создание пользователя")
+        if choice == "1":
+            email = input("Введите email")
+            password = input("Введите password")
+            if email == user_check(session, email):
+                logger.info("Пользователь существует, введите другой email")
+                continue
+            #password = input("Введите password")
+
+            elif user == create_user(session, email, password):
+                logger.info(f"User #{user.id} is created")
+
+
+
+        elif choice == "2":
+            email = input("Введите email")
+            user = find_user(session, email)
+            logger.info(f"User #{user.id}")
+
+        elif choice == "4":
+            user = input("Введите user.id")
+            city = input("Введите ваш город")
+            address = input("Введите ваш адрес")
+            user = user_address(session, user, city, address)
+            logger.info(f"User #{user.id} is created")
+
+        elif choice == "5":
+            user_id = input("Введите user.id")
+            phone = input("Введите номер телефона")
+            age = input("Введите ваш возраст")
+
+        elif choice == "3":
+            exit()
+
--- lesson_14/utils.py
+++ lesson_14/utils.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 from lesson_13.models import Base
+from lesson_13.models import User, Address, Profile
 
 
 def create_tables(engine):
@@ -7,3 +8,33 @@ def create_tables(engine):
     Session = sessionmaker(bind=engine)
     return Session()
 
+
+def create_user(session, email, password):
+    user = User(email=email, password=password)
+
+    session.add(user)
+    session.commit()
+
+    return user
+
+
+def find_user(session, email):
+    return session.query(User).filter(User.email == email).first()
+
+
+def user_check(session, email: str) -> User:
+    return session.query(User).filter(email=email).first()
+
+
+def user_address(session, user, city, address):
+    address = Address(user=user.id, city=city, address=address)
+
+    session.add(address)
+    session.commit()
+
+
+def user_profile(session, user_id, phone, age):
+    profile = Profile(user_id=user.id, phone=phone, age=age)
+
+    session.add(profile)
+    session.commit()
